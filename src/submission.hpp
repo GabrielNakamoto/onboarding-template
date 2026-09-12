c66272414b613ee74527846459c435a46ecd3e61
Gabriel Nakamoto <gabriel@nakamoto.ca>
2026-09-12 15:17:01 +0000

    clean

--- src/submission.hpp
+++ src/submission.hpp
@@ -31,11 +31,8 @@ void apply_stencil(const Grid& __restrict__ old_grid, Grid& __restrict__ new_gri
   memcpy(dst, cells, cols * sizeof(double));
   memcpy(dst+(rows-1)*cols, cells+(rows-1)*cols, cols * sizeof(double));
 
-  #pragma omp parallel for
-  for (std::size_t i=0; i<rows; ++i) {
-    new_grid(i,0)       = old_grid(i,0);
-    new_grid(i,cols-1)  = old_grid(i,cols-1);
-  }
+  new_grid(0,0)       = old_grid(0,0);
+  new_grid(rows-1,cols-1)  = old_grid(rows-1,cols-1);
 
   if (cols < 3 || rows < 3) return;
 
@@ -43,8 +40,10 @@ void apply_stencil(const Grid& __restrict__ old_grid, Grid& __restrict__ new_gri
   const __m256d factor_outer  = _mm256_set1_pd(0.125f);
   const __m256d factor_inner  = _mm256_set1_pd(0.5f);
 
-  #pragma omp parallel for
+  #pragma omp parallel for schedule(static)
   for (std::size_t row=1; row<rows-1; ++row) {
+    new_grid(row,0)       = old_grid(row,0);
+    new_grid(row,cols-1)  = old_grid(row,cols-1);
     for (std::size_t col=1; col<aligned_cols; col+=4) {
       // vectorize patches of 4 (64x4=256 ymm) inner fmas
       __m256d top     = _mm256_loadu_pd(cells + ((row-1)  * cols) + col);
@@ -66,6 +65,8 @@ void apply_stencil(const Grid& __restrict__ old_grid, Grid& __restrict__ new_gri
   // handle misalignment
   if ((cols - 2) % 4 > 0) {
     for (std::size_t row=1; row<rows-1; ++row) {
+      new_grid(row,0)       = old_grid(row,0);
+      new_grid(row,cols-1)  = old_grid(row,cols-1);
       for (std::size_t col=std::max(aligned_cols, (std::size_t)1); col < cols-1; ++col) {
         dst[row*cols + col] = (0.5 * cells[row*cols + col]) + 
                       0.125 * (cells[(row-1)*cols + col] + cells[(row+1)*cols + col]
