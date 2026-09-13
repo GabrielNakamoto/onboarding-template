77840a3cca8f291201490376652efb619a8df4f3
Gabriel Nakamoto <gabriel@nakamoto.ca>
2026-09-13 20:58:08 +0000

    mask intrinsics for padding edge

--- src/submission.hpp
+++ src/submission.hpp
@@ -4,6 +4,11 @@
 #include <vector>
 #include <cstring>
 
+// To try:
+// - aligned buffer allocation to reduce intrinsics overhead
+// - different iteration/loop orders
+// - tiling to improve cache locality
+
 class Grid {
 private:
   std::size_t rows_;
@@ -11,11 +16,11 @@ private:
   std::vector<double> buffer;
 
 public:
-  // TODO: store quantized data and expose precision restoration interface to test harness
   Grid(std::size_t rows, std::size_t cols) : rows_(rows), cols_(cols), buffer(rows*cols, 0.0f) {
     #pragma omp parallel
     {
-      // warmup threads
+      // force thread initializaton/warmup
+      #pragma omp barrier
     }
   }
 
@@ -27,6 +32,19 @@ public:
   double *data()              { return &buffer[0]; }
   const double *data() const  { return &buffer[0]; }
 };
+
+inline static __m256d kernel(__m256d mid, __m256d top, __m256d bottom, __m256d right, __m256d left) {
+  static const __m256d factor_outer = _mm256_set1_pd(0.125f), factor_inner  = _mm256_set1_pd(0.5f);
+
+  // individual muls instead of fmad to avoid dependency chain of acc
+  mid     = _mm256_mul_pd(mid, factor_inner);
+  top     = _mm256_mul_pd(top, factor_outer);
+  bottom  = _mm256_mul_pd(bottom, factor_outer);
+  left    = _mm256_mul_pd(left, factor_outer);
+  right   = _mm256_mul_pd(right, factor_outer);
+
+  return _mm256_add_pd(top, _mm256_add_pd(bottom, _mm256_add_pd(left, _mm256_add_pd(right, mid))));
+}
 
 void apply_stencil(const Grid& __restrict__ old_grid, Grid& __restrict__ new_grid) {
   std::size_t rows = old_grid.get_rows(), cols = old_grid.get_cols();
@@ -42,47 +60,40 @@ void apply_stencil(const Grid& __restrict__ old_grid, Grid& __restrict__ new_gri
 
   if (cols < 3 || rows < 3) return;
 
-  std::size_t aligned_cols = (cols - 2) - ((cols - 2) % 4);
-  const __m256d factor_outer  = _mm256_set1_pd(0.125f);
-  const __m256d factor_inner  = _mm256_set1_pd(0.5f);
-
-  // Block Floating Point (BFP) https://en.wikipedia.org/wiki/Block_floating_point
-  // store values with block shared exponents and 32 bit mantissas individually
-
   #pragma omp parallel for schedule(static)
   for (std::size_t row=1; row<rows-1; ++row) {
     new_grid(row,0)       = old_grid(row,0);
     new_grid(row,cols-1)  = old_grid(row,cols-1);
-    for (std::size_t col=1; col<aligned_cols; col+=4) {
-      // vectorize patches of 4 to reduce across 256bit ymm axis
-      __m256d top     = _mm256_loadu_pd(cells + ((row-1)  * cols) + col);
-      __m256d bottom  = _mm256_loadu_pd(cells + ((row+1)  * cols) + col);
-      __m256d mid     = _mm256_loadu_pd(cells + (row      * cols) + col);
-      __m256d left    = _mm256_loadu_pd(cells + (row      * cols) + col - 1);
-      __m256d right   = _mm256_loadu_pd(cells + (row      * cols) + col + 1);
 
-      // individual muls instead of fmad to avoid dependency chain of acc
-      mid     = _mm256_mul_pd(mid, factor_inner);
-      top     = _mm256_mul_pd(top, factor_outer);
-      bottom  = _mm256_mul_pd(bottom, factor_outer);
-      left    = _mm256_mul_pd(left, factor_outer);
-      right   = _mm256_mul_pd(right, factor_outer);
+    for (std::size_t col=1; col<cols-1; col+=4) {
+      __m256d top, bottom, mid, left, right;
 
-      // reduce together at end
-      mid = _mm256_add_pd(top, _mm256_add_pd(bottom, _mm256_add_pd(left, _mm256_add_pd(right, mid))));
-      _mm256_storeu_pd(dst + row*cols + col, mid);
-    }
-  }
+      // handle unaligned final batch with explicit intrinsics to avoid
+      // handling outside parallelized hot loop
+      if (col + 4 >= cols-1 && (cols - 2) % 4 > 0) {
+        long long masks[4] = {0, 0, 0, 0};
+        for (std::size_t m=0; m<(cols-2)%4; ++m) masks[m] = -1;
 
-  // handle misalignment
-  if ((cols - 2) % 4 > 0) {
-    for (std::size_t row=1; row<rows-1; ++row) {
-      new_grid(row,0)       = old_grid(row,0);
-      new_grid(row,cols-1)  = old_grid(row,cols-1);
-      for (std::size_t col=std::max(aligned_cols, (std::size_t)1); col < cols-1; ++col) {
-        dst[row*cols + col] = (0.5 * cells[row*cols + col]) + 
-                      0.125 * (cells[(row-1)*cols + col] + cells[(row+1)*cols + col]
-                              +cells[row*cols + (col-1)] + cells[row*cols + (col+1)]);
+        // NOTE: reverse order args from memory placement for mask registers
+        __m256i mask = _mm256_set_epi64x(masks[3], masks[2], masks[1], masks[0]);
+
+        top     = _mm256_maskload_pd(cells + ((row-1)  * cols) + col, mask);
+        bottom  = _mm256_maskload_pd(cells + ((row+1)  * cols) + col, mask);
+        mid     = _mm256_maskload_pd(cells + (row      * cols) + col, mask);
+        left    = _mm256_maskload_pd(cells + (row      * cols) + col - 1, mask);
+        right   = _mm256_maskload_pd(cells + (row      * cols) + col + 1, mask);
+
+        __m256d out = kernel(mid, top, bottom, right, left);
+        _mm256_maskstore_pd(dst + row*cols + col, mask, out);
+      } else {
+        top     = _mm256_loadu_pd(cells + ((row-1)  * cols) + col);
+        bottom  = _mm256_loadu_pd(cells + ((row+1)  * cols) + col);
+        mid     = _mm256_loadu_pd(cells + (row      * cols) + col);
+        left    = _mm256_loadu_pd(cells + (row      * cols) + col - 1);
+        right   = _mm256_loadu_pd(cells + (row      * cols) + col + 1);
+
+        __m256d out = kernel(mid, top, bottom, right, left);
+        _mm256_storeu_pd(dst + row*cols + col, out);
       }
     }
   }
